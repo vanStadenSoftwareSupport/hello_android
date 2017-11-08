b299d507920dbffd354252da621a464fe3662279
Grant van Staden <grantv@youi.email>
2017-11-08 10:52:17 +1000

    Added greeter

--- app/src/main/java/name/vanstaden/helloandroid/MainActivity.java
+++ app/src/main/java/name/vanstaden/helloandroid/MainActivity.java
@@ -2,12 +2,18 @@ package name.vanstaden.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import name.vanstaden.greetinglib.Greeter;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActvity",
+            WHO = "User";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView greetingTextView = findViewById(R.id.txtGreeting);
+        greetingTextView.setText(new Greeter().greet(WHO));
     }
 }
